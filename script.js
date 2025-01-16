72387293ba24a46574eb4b30d736d044f7f91b1d
Juandvr <villabonajuan203@gmail.com>
2025-01-16 13:45:40 -0500

    fix cells size

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+const defaultSize = 16;
+
+let curSize = defaultSize;
+
+const size = document.getElementById('size');
+
 function createGrid() {
-    const container = document.getElementById("container");
+    const container = document.getElementById('container');
+    container.innerHTML = '';
     
-    for(let i = 0; i < 256; i++) {
-        const grid = document.createElement("div");
-        grid.setAttribute("class", "grid");
-        grid.addEventListener("mouseover", () => {grid.classList.toggle("draw")});
-        container.appendChild(grid);
+    for(let i = 0; i < curSize; i++) {
+        for(let j = 0; j < curSize; j++) {
+            const grid = document.createElement('div');
+            grid.setAttribute('class', 'cell');
+            grid.addEventListener('mouseover', () => {grid.classList.toggle('draw')});
+            container.appendChild(grid);
+        }
     }
+
+    document.documentElement.style.setProperty('--curWidth', curSize);
+    document.documentElement.style.setProperty('--curHeight', curSize);
 }
 
-window.addEventListener("load", createGrid);
+window.addEventListener('load', createGrid);
+
+size.addEventListener('change', () => {
+    curSize = size.value;
+    createGrid();
+})
+
